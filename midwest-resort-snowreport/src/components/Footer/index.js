584527ad14f636be23dfac9efe6e37f8ad2b14fc
Phil Dugliss <phillipdugliss@gmail.com>
2019-10-22 13:16:45 -0500

    Footer Added
    -Footer Component added
    -Fixed Positioning in Navbar added

---
+++ midwest-resort-snowreport/src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function Footer() {
+    return (  
+        <nav class="navbar navbar-light bg-light">
+            <p>this is a footer</p>
+        <a class="navbar-brand" href="#"></a>
+        </nav>
+    );
+}
+
+export default Footer
